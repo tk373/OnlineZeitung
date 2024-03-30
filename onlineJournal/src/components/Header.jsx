58025bb2c13253da33b1732ea68f98ad6cdf33aa
ArlesEST <130912980+ArlesEST@users.noreply.github.com>
2024-03-30 16:37:44 +0100

    gesamter button im header clickable

--- onlineJournal/src/components/Header.jsx
+++ onlineJournal/src/components/Header.jsx
@@ -4,19 +4,19 @@ import './Header.css'
 function Header() {
   return (
     <header className="header">
-    <div className="header-align-left">
-        <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100" />
-        <h1>D'POSCHT</h1>
-    </div>
-     <div className="header-align-right">
-     <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="./About">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-     </div>
+      <div className="header-align-left">
+          <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100" />
+          <h1>D'POSCHT</h1>
+      </div>
+       <div className="header-align-right">
+       <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="./About">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+       </div>
     </header>
   );
 }
