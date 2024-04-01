316bea50fcffcfa9b36f13beefc83d59e3b54065
ArlesEST <130912980+ArlesEST@users.noreply.github.com>
2024-04-01 17:24:28 +0200

    komplett neues design logo = homelink about page neues design + Inhalt

--- onlineJournal/src/components/Header.jsx
+++ onlineJournal/src/components/Header.jsx
@@ -5,15 +5,16 @@ function Header() {
   return (
     <header className="header">
       <div className="header-align-left">
-          <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100" />
-          <h1>D'POSCHT</h1>
+        <a href="./">
+          <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100"/>
+        </a>
       </div>
        <div className="header-align-right">
        <nav>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="./About">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="./">Diheime</a></li>
+            <li><a href="./About">Über eus</a></li>
+            <li><a href="#">Kontakt</a></li>
           </ul>
         </nav>
        </div>
