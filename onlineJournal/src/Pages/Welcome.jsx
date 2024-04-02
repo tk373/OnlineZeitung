650419691d02c6ce792a46ff2a7b8d85c3b49688
ArlesEST <130912980+ArlesEST@users.noreply.github.com>
2024-04-02 17:13:52 +0200

    homepage

--- onlineJournal/src/Pages/Welcome.jsx
+++ onlineJournal/src/Pages/Welcome.jsx
@@ -3,7 +3,7 @@ import './Welcome.css'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
 import supabase from '../supabaseClient.js'
-import {CardBody, CardHeader, Card} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 
 function Welcome() {
   
@@ -34,17 +34,25 @@ function Welcome() {
         <Header></Header>
       </header>
       <div className="mainContent">
-        <div className="ContentTitel">
-            <h1>
-                Herzlich willkomme uf de Homepage vo de Poscht
-            </h1>
-            <p>
-                Ihrer vertrauenswürdigen Quelle für Nachrichten und Analysen. Bei uns finden Sie tiefgehende Berichterstattung zu den wichtigsten Themen unserer Zeit – von lokalen Ereignissen bis hin zu globalen Entwicklungen. 
-            </p>
-            <p>
-            Wir sind stolz darauf, Ihnen eine Plattform bieten zu können, auf der Journalismus noch echte Bedeutung hat. Entdecken Sie mit uns die Geschichten, die die Welt bewegen. Vielen Dank, dass Sie uns gewählt haben, um informiert zu bleiben.
-            </p>
-        </div>
+      <Card className="max-w-[80vw]">
+  <CardHeader className="flex gap-3">
+    <div className="flex flex-col">
+      <p className="text-7xl font-bold">Herzlich willkomme uf de Homepage vo de Poscht</p>
+    </div>
+  </CardHeader>
+  <Divider/>
+  <CardBody>
+    <p className="text-2xl">
+      Ihrer vertrauenswürdigen Quelle für Nachrichten und Analysen. Bei uns finden Sie tiefgehende Berichterstattung zu den wichtigsten Themen unserer Zeit – von lokalen Ereignissen bis hin zu globalen Entwicklungen. 
+      <br></br>
+      <br></br>
+      Wir sind stolz darauf, Ihnen eine Plattform bieten zu können, auf der Journalismus noch echte Bedeutung hat. Entdecken Sie mit uns die Geschichten, die die Welt bewegen. Vielen Dank, dass Sie uns gewählt haben, um informiert zu bleiben.
+      </p>
+    
+  </CardBody>
+</Card>
+
+        
       </div>
       <div className="articlesContainer">
         {articles.map((article) => (
