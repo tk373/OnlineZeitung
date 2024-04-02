53e85411ff6331887ee3d3d81aab8992ab902579
ArlesEST <130912980+ArlesEST@users.noreply.github.com>
2024-04-02 17:41:41 +0200

    header falsch farbe

--- onlineJournal/src/Pages/Welcome.jsx
+++ onlineJournal/src/Pages/Welcome.jsx
@@ -3,10 +3,10 @@ import './Welcome.css'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
 import supabase from '../supabaseClient.js'
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
 
 function Welcome() {
-  
+
   const [articles, setArticles] = useState([]);
 
   // Funktion zum Abrufen der Artikel aus Supabase
@@ -34,40 +34,38 @@ function Welcome() {
         <Header></Header>
       </header>
       <div className="mainContent">
-      <Card className="max-w-[80vw]">
-  <CardHeader className="flex gap-3">
-    <div className="flex flex-col">
-      <p className="text-7xl font-bold">Herzlich willkomme uf de Homepage vo de Poscht</p>
-    </div>
-  </CardHeader>
-  <Divider/>
-  <CardBody>
-    <p className="text-2xl">
-      Ihrer vertrauenswürdigen Quelle für Nachrichten und Analysen. Bei uns finden Sie tiefgehende Berichterstattung zu den wichtigsten Themen unserer Zeit – von lokalen Ereignissen bis hin zu globalen Entwicklungen. 
-      <br></br>
-      <br></br>
-      Wir sind stolz darauf, Ihnen eine Plattform bieten zu können, auf der Journalismus noch echte Bedeutung hat. Entdecken Sie mit uns die Geschichten, die die Welt bewegen. Vielen Dank, dass Sie uns gewählt haben, um informiert zu bleiben.
-      </p>
-    
-  </CardBody>
-</Card>
+        <Card className="max-w-[80vw]">
+          <CardHeader className="flex gap-3">
+            <div className="flex flex-col">
+              <p className="text-7xl font-bold">Herzlich willkomme uf de Homepage vo de Poscht</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p className="text-2xl">
+              Ihrer vertrauenswürdigen Quelle für Nachrichten und Analysen. Bei uns finden Sie tiefgehende Berichterstattung zu den wichtigsten Themen unserer Zeit – von lokalen Ereignissen bis hin zu globalen Entwicklungen.
+              <br></br>
+              <br></br>
+              Wir sind stolz darauf, Ihnen eine Plattform bieten zu können, auf der Journalismus noch echte Bedeutung hat. Entdecken Sie mit uns die Geschichten, die die Welt bewegen. Vielen Dank, dass Sie uns gewählt haben, um informiert zu bleiben.
+            </p>
 
-        
+          </CardBody>
+        </Card>
       </div>
       <div className="articlesContainer">
         {articles.map((article) => (
-       <Card key={article.id} hoverable clickable>
-        <CardHeader>
-          <h3>{article.title}</h3>
-        </CardHeader>
-        <CardBody>
-          <p>{article.lead}</p>
-          <div>{article.body}</div>
-        </CardBody>
-      </Card>
-      ))}
+          <Card key={article.id} hoverable clickable>
+            <CardHeader>
+              <h3>{article.title}</h3>
+            </CardHeader>
+            <CardBody>
+              <p>{article.lead}</p>
+              <div>{article.body}</div>
+            </CardBody>
+          </Card>
+        ))}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- onlineJournal/src/components/Header.jsx
+++ onlineJournal/src/components/Header.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Header.css'
+import { Button } from "@nextui-org/react"
 
 function Header() {
   return (
     <header className="header">
       <div className="header-align-left">
         <a href="./">
-          <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100"/>
+          <img className="Logo" src="./Logo2.svg" alt="Logo" width="70" height="100" />
         </a>
       </div>
-       <div className="header-align-right">
-       <nav>
-          <ul>
-            <li><a href="./">Diheime</a></li>
-            <li><a href="./About">Über eus</a></li>
-            <li><a href="#">Kontakt</a></li>
-            <li><a href="./Abo">Abo</a></li>
-            <li><a href="./Add">Hinzufügen</a></li>
-          </ul>
-        </nav>
-       </div>
+      <div className="header-align-right">
+        <div className='flex gap-4 items-center'>
+          <Button className='nav-button' variant="ghost" size="lg" radius="sm" as="a" href="./">Diheime</Button>
+        <Button className='nav-button' variant="ghost" size="lg" radius="sm" as="a" href="./About">Über eus</Button>
+        <Button className='nav-button' variant="ghost" size="lg" radius="sm" as="a" href="#">Kontakt</Button>
+        <Button className='nav-button' variant="ghost" size="lg" radius="sm" as="a" href="./Abo">Abo</Button>
+        <Button className='nav-button' variant="ghost" size="lg" radius="sm" as="a" href="./Add">Hinzufügen</Button>
+        </div>
+        
+      </div>
     </header>
   );
 }
