ffffa772c886326ed4a93f4d4a63f24abee58d7d
tk373 <silva.hegner@gmail.com>
2024-04-24 13:25:30 +0200

    env didnt work

--- src/Pages/Generate.jsx
+++ src/Pages/Generate.jsx
@@ -5,14 +5,11 @@ import configData from '../../config.json';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import { Card, CardHeader, CardBody, Tabs, Tab, Accordion, AccordionItem, Input, Button, Textarea } from "@nextui-org/react";
-import dotenv from 'dotenv';
 
 function Generate() {
     const [articlePrompt, setArticlePrompt] = useState('');
     const [sourceURLs, setSourceURLs] = useState({ 1: '', 2: '', 3: '' });
     const [generatedArticle, setGeneratedArticle] = useState('Hier wird dein generierter Artikel angezeigt.');
-    dotenv.config();
-    const apiKey = process.env.API_KEY;
     const headers = {
 
     }
@@ -27,7 +24,7 @@ function Generate() {
 
     const generateArticle = async () => {
         const apiURL = 'https://api.openai.com/v1/chat/completions';
-       // const apiKey = configData.API_KEY;
+       const apiKey = configData.API_KEY;
        console.log('My API Key:', apiKey);
         const config = {
             headers: {
