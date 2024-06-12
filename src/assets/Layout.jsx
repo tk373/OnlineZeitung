3b3db88bab8d712f02b797a361a8a767b7ea05ca
tk373 <silva.hegner@gmail.com>
2024-06-12 18:22:31 +0200

    LayoutFixed?

--- src/assets/Layout.jsx
+++ src/assets/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }) => {
   return (
-      <div className='layoutContainer' style={{ width: '80vw', margin: '0 auto', backgroundColor: 'var(--background)', minHeight: "100%"}}>
+      <div className='layoutContainer' style={{ width: '1100px', margin: '0 auto', backgroundColor: 'var(--background)', minHeight: "100%"}}>
       {children}
       </div>
   );
